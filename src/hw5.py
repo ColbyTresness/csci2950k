c98cb3a22f5ab6fa6a7c617a0c42a8088ed6456c
ColbyTresness <colby_tresness@brown.edu>
2016-10-24 09:27:13 -0400

    fixed hw5 code

--- src/hw5.py
+++ src/hw5.py
@@ -23,28 +23,33 @@ def basic_tokenizer(sentence, word_split=re.compile(b"([.,!?\"':;)(])")):
 book = open(BOOKPATH, "rb")
 
 vocab = basic_tokenizer(book.read())
-
 vocabCounts = Counter(vocab)
 
 wordIndices = {}
 
 counter = 1
 for word in vocabCounts:
-	if counter < 8001:
+	if counter < 8000:
 		wordIndices[word] = counter
 	else:
-		wordIndices[word] = 8001
+		wordIndices[word] = 8000
 	counter += 1
 
-trainWords = []
-testWords = []
+trainWords = list()
+testWords = list()
 
-vocabSize = len(vocab)
-for i in range(vocabSize):
-	if i < math.floor(vocabSize*.9):
-		trainWords += vocab[i]
+vocabSize = maxVocabSize + 1
+i = 0
+for word in vocab:
+	i += 1
+	if i < math.floor(len(vocab)*.9):
+		trainWords.append(wordIndices[word])
 	else:
-		testWords += vocab[i]
+		testWords.append(wordIndices[word])
+
+trains = np.array(trainWords)
+tests = np.array(testWords)
+
 
 # constants
 EMBEDSIZE = 50
@@ -59,14 +64,15 @@ print("creating model")
 x = tf.placeholder(tf.int32, [BATCHSIZE, NUMSTEPS])
 y = tf.placeholder(tf.int32, [BATCHSIZE, NUMSTEPS])
 keep_prob = tf.placeholder(tf.float32)
+
 E = tf.Variable(tf.random_uniform([vocabSize, EMBEDSIZE], minval=-1, maxval=1, dtype=tf.float32, seed=0))
 embed = tf.nn.embedding_lookup(E, x)
 
-basicLSTMCell = tf.nn.rnn_cell.BasicLSTMCell(LSTMSIZE)
+basicLSTMCell = tf.nn.rnn_cell.BasicLSTMCell(LSTMSIZE, state_is_tuple=True)
+initialState = basicLSTMCell.zero_state(BATCHSIZE, tf.float32)
 
-initialState = basicLSTMCell.zero_state(BATCHSIZE, dtype=tf.float32)
 w = tf.Variable(tf.truncated_normal([LSTMSIZE, vocabSize], stddev=0.1))
-b = tf.Variable(tf.constant(0.1, shape=[1, vocabSize]))
+b = tf.Variable(tf.constant(0.1, shape=[vocabSize]))
 
 embedDrop = tf.nn.dropout(embed, keep_prob)
 
@@ -76,64 +82,64 @@ rnn2 = tf.reshape(rnn, [BATCHSIZE * NUMSTEPS, LSTMSIZE])
 
 logits = tf.matmul(rnn2, w) + b
 
-wgts = tf.Variable(tf.constant(1.0, shape=[1, BATCHSIZE*NUMSTEPS]))
-y = tf.reshape(y, [1, BATCHSIZE*NUMSTEPS])
-loss1 = tf.nn.seq2seq.sequence_loss_by_example([logits], [y], [wgts])
+wgts = tf.Variable(tf.constant(1.0, shape=[BATCHSIZE*NUMSTEPS]))
+y1d = tf.reshape(y, [BATCHSIZE*NUMSTEPS])
+
+loss1 = tf.nn.seq2seq.sequence_loss_by_example([logits], [y1d], [wgts])
 loss = tf.reduce_sum(loss1)
-perplexity = tf.exp(loss / BATCHSIZE)
 
 # set up training step
-train_step = tf.train.AdamOptimizer(TRAININGRATE).minimize(perplexity)
+trainStep = tf.train.AdamOptimizer(TRAININGRATE).minimize(loss)
 
 # initialize
 sess.run(tf.initialize_all_variables())
 
 print("training")
 # train
+lsum = 0
+iters = 0
 for e in range(EPOCHS):
-	x = 0
-	state = initialState.eval()
-	while x + BATCHSIZE*NUMSTEPS + 1 < len(trainWords):
-		print("x: " + str(x))
-		print(type(state))
-		print(state)
-		nextstate, perp = sess.run([outst, perplexity], 
-			feed_dict = {x: trainWords[x : x + BATCHSIZE*NUMSTEPS],
-						 y: trainWords[x+1 : x+1+BATCHSIZE*NUMSTEPS], 
-						 keep_prob: .5, 
+	i = 0
+	state = (np.zeros([BATCHSIZE, LSTMSIZE]), np.zeros([BATCHSIZE, LSTMSIZE]))
+	while i + BATCHSIZE*NUMSTEPS + 1 < len(trains):
+		iters += 1
+		nextstate, _, l = sess.run([outst, trainStep, loss], 
+			feed_dict = {x: np.reshape(trains[i : i + BATCHSIZE*NUMSTEPS], (BATCHSIZE, NUMSTEPS)),
+						 y: np.reshape(trains[i+1 : i+1+BATCHSIZE*NUMSTEPS], (BATCHSIZE, NUMSTEPS)),  
+						 keep_prob: .5,
 						 initialState: state})
-		if x % 5000 == 0:
-			print("perplexity for " + str(x) + ": %g"%perplexity.eval(feed_dict = {x: trainWords[x : x + BATCHSIZE*NUMSTEPS], 
-																				y: trainWords[x+1 : x+1+BATCHSIZE*NUMSTEPS], 
-																				keep_prob: .5, 
-						 														initialState: state}))
-		x += BATCHSIZE
+		print(math.exp(l/(BATCHSIZE*NUMSTEPS)))
+		i += BATCHSIZE*NUMSTEPS
+		lsum += (l / (BATCHSIZE*NUMSTEPS))
 		state = nextstate
+print("train perplexity: ")
+print(math.exp(lsum/iters))
+print("lsum: ")
+print(lsum)
+print("iters: ")
+print(iters)
+print("lsum/iters: ")
+print(lsum/iters)
 
 
 print("testing")
 # test
+iters = 0
+lsum = 0
 for e in range(EPOCHS):
-	x = 0
-	state = (initialState[0].eval(), initialState[1].eval())
-	while x + BATCHSIZE*NUMSTEPS + 1 < len(trainWords):
-		nextstate, perp = sess.run([outst, perplexity], 
-			feed_dict = {x: testWords[x : x + BATCHSIZE*NUMSTEPS],
-						 y: testWords[x+1 : x+1+BATCHSIZE*NUMSTEPS], 
+	i = 0
+	state = (np.zeros([BATCHSIZE, LSTMSIZE]), np.zeros([BATCHSIZE, LSTMSIZE]))
+	while i + BATCHSIZE*NUMSTEPS + 1 < len(tests):
+		iters += 1
+		nextstate, _, l = sess.run([outst, trainStep, loss], 
+			feed_dict = {x: np.reshape(tests[i : i + BATCHSIZE*NUMSTEPS], (BATCHSIZE, NUMSTEPS)),
+						 y: np.reshape(tests[i+1 : i+1+BATCHSIZE*NUMSTEPS], (BATCHSIZE, NUMSTEPS)),
 						 keep_prob: 1.0, 
 						 initialState: state})
-		if x % 5000 == 0:
-			print("perplexity for " + str(x) + ": %g"%perplexity.eval(feed_dict = {x: testWords[x : x + BATCHSIZE*NUMSTEPS], 
-																				y: testWords[x+1 : x+1+BATCHSIZE*NUMSTEPS], 
-																				keep_prob: .5,
-						 														initialState: state}))
-		x += BATCHSIZE
+		print(math.exp(l/(BATCHSIZE*NUMSTEPS)))
+		i += BATCHSIZE*NUMSTEPS
 		state = nextstate
+		lsum += (l / (BATCHSIZE*NUMSTEPS))
+print("test perplexity: ")
+print(math.exp(lsum/iters))
 
-
-
-
-# err = sess.run(error, feed_dict = {x: trainWords[0 : len(testWords)-1], y: trainWords[1 : len(testWords)], keep_prob: 1.0, initialState[0]: state[0], initialState[1]: initial_state[1]})
-# print("test perplexity %g"%perplexity.eval(feed_dict = {x: testWords[0 : len(testWords)-1], y: testWords[1 : len(testWords)], keep_prob: 1.0, initialState[0]: initial_state[0], initialState[1]: initial_state[1]}))
-
-# 293.386
